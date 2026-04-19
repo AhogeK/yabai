18105f58c5f17501e8d4d8541f599092d32d56c6
Åsmund Vikane <aasvi93@hotmail.com>
2026-04-19 15:23:48 +0200

    #2781 bypass space switch animation on cmd+tab with SIP enabled

--- src/space_manager.c
+++ src/space_manager.c
@@ -899,16 +899,24 @@ enum space_op_error space_manager_move_space_to_display(struct space_manager *sm
     return SPACE_OP_ERROR_SCRIPTING_ADDITION;
 }
 
-static bool space_manager_focus_space_using_gesture(uint32_t new_did, uint64_t new_sid)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+bool space_manager_focus_space_using_gesture(uint32_t new_did, uint64_t new_sid)
 {
-    uint32_t cur_did = display_manager_cursor_display_id();
-    if (cur_did != new_did) {
-        CGWarpMouseCursorPosition(display_center(new_did));
+    int cur_index = space_manager_mission_control_index(display_space_id(new_did));
+    int new_index = space_manager_mission_control_index(new_sid);
+
+    int count = abs(new_index - cur_index);
+    if (count == 0) {
+        display_manager_focus_display(new_did, new_sid);
+        return true;
     }
 
-    uint64_t cur_sid = display_space_id(new_did);
-    int cur_index = space_manager_mission_control_index(cur_sid);
-    int new_index = space_manager_mission_control_index(new_sid);
+    CGPoint point = display_center(new_did);
+    uint32_t cur_did = display_manager_cursor_display_id();
+
+    bool focus_display = cur_did != new_did;
+    if (focus_display) CGWarpMouseCursorPosition(point);
 
     //
     // NOTE(asmvik): MacOS does not have an API that allows for space activation.
@@ -929,21 +937,27 @@ static bool space_manager_focus_space_using_gesture(uint32_t new_did, uint64_t n
     CGEventSetIntegerValueField(event_dock_control, /* kCGEventGestureHIDType        */ 110, /* kIOHIDEventTypeDockSwipe   */ 23);
     CGEventSetIntegerValueField(event_dock_control, /* kCGEventGestureSwipeMotion    */ 123, /* kCGGestureMotionHorizontal */  1);
     CGEventSetDoubleValueField(event_dock_control,  /* kCGEventGestureSwipeProgress  */ 124, sign);
-    CGEventSetDoubleValueField(event_dock_control,  /* kCGEventGestureSwipeVelocityX */ 129, sign * 999.0);
+    CGEventSetDoubleValueField(event_dock_control,  /* kCGEventGestureSwipeVelocityX */ 129, sign * 9999.0);
 
-    int count = abs(new_index - cur_index);
     for (int i = 0; i < count; ++i) {
         CGEventSetIntegerValueField(event_dock_control, /* kCGEventGesturePhase */ 132, /* kCGSGesturePhaseBegan */ 1);
         CGEventPost(kCGSessionEventTap, event_dock_control);
         CGEventSetIntegerValueField(event_dock_control, /* kCGEventGesturePhase */ 132, /* kCGSGesturePhaseEnded */ 4);
         CGEventPost(kCGSessionEventTap, event_dock_control);
     }
-
     CFRelease(event_dock_control);
-    display_manager_focus_display(new_did, new_sid);
+
+    if (focus_display) {
+        display_manager_set_active_display_id(new_did);
+        if (space_manager_active_space() != new_sid) {
+            CGPostMouseEvent(point, false, 1, true);
+            CGPostMouseEvent(point, false, 1, false);
+        }
+    }
 
     return true;
 }
+#pragma clang diagnostic pop
 
 enum space_op_error space_manager_focus_space(uint64_t sid)
 {
